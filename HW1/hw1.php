d8fec34fae00aa3165f9544ca6f5fdddb635f6f7
bbernatann <122397159+bbernatann@users.noreply.github.com>
2023-05-31 18:15:38 +0300

    Add files via upload

---
+++ HW1/hw1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hyperlink Examples</title>
+</head>
+<body>
+<h1>Hyperlink Examples</h1>
+
+<!-- Link to another site -->
+<p><a href="https://www.example.com">Visit Example Website</a></p>
+
+<!-- Anchor to a page on the same site -->
+<p><a href="#section2">Jump to Section 2</a></p>
+
+<!-- Link that dials a number -->
+<p><a href="tel:+1234567890">Call us at +1 234-567-890</a></p>
+
+<!-- Link that runs JavaScript -->
+<p><a href="javascript:alert('Hello, world!')">Click to display an alert</a></p>
+
+<!-- Link that opens an email client -->
+<p><a href="mailto:info@example.com">Email us at info@example.com</a></p>
+
+<!-- Open the link in a new tab/window -->
+<p><a href="https://www.example.com" target="_blank">Visit Example Website (opens in new tab)</a></p>
+
+<h2 id="section2">Section 2</h2>
+<p>This is Section 2 of the page.</p>
+</body>
+</html>
+
